aa8191853fbfe4e858d043282d31b608cc56114d
mike <mxu@capvision.com>
2024-09-11 10:54:20 +0800

    add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  // https://vitejs.dev/guide/static-deploy#github-pages
+  base: '/slides-math-in-coding/',
   plugins: [react()],
   build: {
     outDir: 'dist',
